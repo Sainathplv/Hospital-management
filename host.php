eed663015ccf1ad09d83f4b903e3048e63d53d5b
SAINATH REDDY PALAVALA <107321145+Sainathplv@users.noreply.github.com>
2022-07-06 14:08:02 -0500

    Add files via upload

---
+++ host.php
@@ -0,0 +1,19 @@
+﻿$link = new mysqli("localhost","username,"password","db name");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  die ("There was an error connecting to the database");
+       
+  exit();
+}
+        $query = "INSERT INTO `users` (`your values`, `yourvalues`) VALUES ('".mysqli_real_escape_string($link, $_POST['yourvalue'])."', '".mysqli_real_escape_string($link, $_POST['yourvalue])."')";
+               
+                mysqli_query($link, $query);
+        }    
+       
+    }
+   
+   
+
+?>
